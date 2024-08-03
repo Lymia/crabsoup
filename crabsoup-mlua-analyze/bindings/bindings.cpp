b2463736583eb2bc5b8dce89fdbc0e441cbe2a86
Rimi Kanokawa <rimi@rimin.moe>
2024-08-02 21:13:53 -0700

    Format with clang-format

--- crabsoup-mlua-analyze/bindings/bindings.cpp
+++ crabsoup-mlua-analyze/bindings/bindings.cpp
@@ -1,5 +1,5 @@
-#include "Luau/Frontend.h"
 #include "Luau/BuiltinDefinitions.h"
+#include "Luau/Frontend.h"
 
 namespace luauAnalyze {
     struct SourceInfo {
@@ -10,7 +10,7 @@ namespace luauAnalyze {
     struct MemoryFileResolver : Luau::FileResolver {
         std::unordered_map<std::string, SourceInfo> sources;
 
-        virtual std::optional<Luau::SourceCode> readSource(const Luau::ModuleName& name) {
+        virtual std::optional<Luau::SourceCode> readSource(const Luau::ModuleName &name) {
             auto it = sources.find(name);
             if (it == sources.end()) {
                 return std::nullopt;
@@ -19,13 +19,13 @@ namespace luauAnalyze {
                 sources.erase(it);
 
                 auto type = source.is_module ? Luau::SourceCode::Type::Module : Luau::SourceCode::Type::Script;
-                Luau::SourceCode info = { std::move(source.code), type };
+                Luau::SourceCode info = {std::move(source.code), type};
                 return std::move(info);
             }
         }
 
         void register_source(std::string name, std::string source, bool is_module) {
-            sources[name] = { source, is_module };
+            sources[name] = {source, is_module};
         }
     };
 
@@ -34,9 +34,9 @@ namespace luauAnalyze {
         std::unique_ptr<Luau::NullConfigResolver> config_resolver;
         std::unique_ptr<Luau::Frontend> frontend;
     };
-}
+} // namespace luauAnalyze
 
-[[noreturn]] static void error(const char* error) {
+[[noreturn]] static void error(const char *error) {
     printf("internal error in crabsoup-mlua-analyze: %s\n", error);
     throw std::runtime_error("err");
 }
@@ -44,59 +44,37 @@ namespace luauAnalyze {
 extern "C" {
     struct RustCheckResultReceiver;
     struct RustString {
-        const char* data;
+        const char *data;
         size_t len;
     };
     struct RustLineColumn {
         unsigned int line, column;
     };
-    extern void luauAnalyze_push_result(
-        RustCheckResultReceiver* receiver,
-        RustString module,
-        RustLineColumn error_start,
-        RustLineColumn error_end,
-        bool is_error,
-        bool is_lint,
-        RustString message
-    );
+    extern void luauAnalyze_push_result(RustCheckResultReceiver *receiver, RustString module,
+                                        RustLineColumn error_start, RustLineColumn error_end, bool is_error,
+                                        bool is_lint, RustString message);
 
-    static RustString to_rust_string(std::string& str) {
-        return { str.data(), str.length() };
+    static RustString to_rust_string(std::string &str) {
+        return {str.data(), str.length()};
     }
     static std::string from_rust_string(RustString str) {
         std::string new_str(str.data, str.len);
         return new_str;
     }
-    static void push_to_receiver(RustCheckResultReceiver* receiver, Luau::TypeError& error) {
+    static void push_to_receiver(RustCheckResultReceiver *receiver, Luau::TypeError &error) {
         auto message = toString(error);
         luauAnalyze_push_result(
-            receiver,
-            to_rust_string(error.moduleName),
-            { error.location.begin.line, error.location.begin.column },
-            { error.location.end.line, error.location.end.column },
-            true,
-            false,
-            to_rust_string(message)
-        );
+            receiver, to_rust_string(error.moduleName), {error.location.begin.line, error.location.begin.column},
+            {error.location.end.line, error.location.end.column}, true, false, to_rust_string(message));
     }
-    static void push_to_receiver_lint(
-        RustCheckResultReceiver* receiver,
-        std::string& name,
-        Luau::LintWarning& error,
-        bool is_error
-    ) {
+    static void push_to_receiver_lint(RustCheckResultReceiver *receiver, std::string &name, Luau::LintWarning &error,
+                                      bool is_error) {
         luauAnalyze_push_result(
-            receiver,
-            to_rust_string(name),
-            { error.location.begin.line, error.location.begin.column },
-            { error.location.end.line, error.location.end.column },
-            is_error,
-            true,
-            to_rust_string(error.text)
-        );
+            receiver, to_rust_string(name), {error.location.begin.line, error.location.begin.column},
+            {error.location.end.line, error.location.end.column}, is_error, true, to_rust_string(error.text));
     }
 
-    luauAnalyze::FrontendWrapper* luauAnalyze_new_frontend() {
+    luauAnalyze::FrontendWrapper *luauAnalyze_new_frontend() {
         auto file_resolver = std::make_unique<luauAnalyze::MemoryFileResolver>();
         auto config_resolver = std::make_unique<Luau::NullConfigResolver>();
 
@@ -114,20 +92,13 @@ extern "C" {
         return wrapper;
     }
 
-    bool luauAnalyze_register_definitions(
-        luauAnalyze::FrontendWrapper* wrapper,
-        RustString r_module_name,
-        RustString r_definitions
-    ) {
+    bool luauAnalyze_register_definitions(luauAnalyze::FrontendWrapper *wrapper, RustString r_module_name,
+                                          RustString r_definitions) {
         auto module_name = from_rust_string(r_module_name);
         auto definitions = from_rust_string(r_definitions);
-        auto result = wrapper->frontend->loadDefinitionFile(
-            wrapper->frontend->globals,
-            wrapper->frontend->globals.globalScope,
-            std::move(definitions),
-            std::move(module_name),
-            false
-        );
+        auto result =
+            wrapper->frontend->loadDefinitionFile(wrapper->frontend->globals, wrapper->frontend->globals.globalScope,
+                                                  std::move(definitions), std::move(module_name), false);
         return result.success;
     }
 
@@ -137,15 +108,15 @@ extern "C" {
         std::vector<std::string> res;
 
         while ((pos_end = s.find(delimiter, pos_start)) != std::string::npos) {
-            token = s.substr (pos_start, pos_end - pos_start);
+            token = s.substr(pos_start, pos_end - pos_start);
             pos_start = pos_end + delim_len;
-            res.push_back (token);
+            res.push_back(token);
         }
 
-        res.push_back (s.substr (pos_start));
+        res.push_back(s.substr(pos_start));
         return res;
     }
-    static bool add_deprecation_to_table(Luau::TableType* ttv, std::string& target, std::string& replacement) {
+    static bool add_deprecation_to_table(Luau::TableType *ttv, std::string &target, std::string &replacement) {
         if (ttv) {
             if (ttv->props.count(target)) {
                 ttv->props[target].deprecated = true;
@@ -157,11 +128,8 @@ extern "C" {
             return false;
         }
     }
-    void luauAnalyze_set_deprecation(
-        luauAnalyze::FrontendWrapper* wrapper,
-        RustString r_module_path,
-        RustString r_replacement
-    ) {
+    void luauAnalyze_set_deprecation(luauAnalyze::FrontendWrapper *wrapper, RustString r_module_path,
+                                     RustString r_replacement) {
         auto module_path = from_rust_string(r_module_path);
         auto replacement = from_rust_string(r_replacement);
         bool has_replacement = replacement != "";
@@ -174,14 +142,14 @@ extern "C" {
                 wrapper->frontend->globals.globalScope->bindings[astName].deprecatedSuggestion = replacement;
         } else if (split.size() == 2) {
             auto binding = Luau::getGlobalBinding(wrapper->frontend->globals, split[0]);
-            Luau::TableType* ttv = Luau::getMutable<Luau::TableType>(binding);
+            Luau::TableType *ttv = Luau::getMutable<Luau::TableType>(binding);
             if (ttv) {
                 add_deprecation_to_table(ttv, module_path, replacement);
             } else {
-                Luau::IntersectionType* intersection = Luau::getMutable<Luau::IntersectionType>(binding);
+                Luau::IntersectionType *intersection = Luau::getMutable<Luau::IntersectionType>(binding);
                 if (intersection) {
-                    for (auto& entry : intersection->parts) {
-                        Luau::TableType* e_ttv = Luau::getMutable<Luau::TableType>(entry);
+                    for (auto &entry : intersection->parts) {
+                        Luau::TableType *e_ttv = Luau::getMutable<Luau::TableType>(entry);
                         add_deprecation_to_table(e_ttv, module_path, replacement);
                     }
                 } else {
@@ -193,32 +161,30 @@ extern "C" {
         }
     }
 
-    void luauAnalyze_freeze_definitions(luauAnalyze::FrontendWrapper* wrapper) {
+    void luauAnalyze_freeze_definitions(luauAnalyze::FrontendWrapper *wrapper) {
         Luau::freeze(wrapper->frontend->globals.globalTypes);
         Luau::freeze(wrapper->frontend->globalsForAutocomplete.globalTypes);
     }
 
-    void luauAnalyze_check(
-        RustCheckResultReceiver* receiver,
-        luauAnalyze::FrontendWrapper* wrapper,
-        RustString r_name,
-        RustString r_contents,
-        bool is_module
-    ) {
+    void luauAnalyze_check(RustCheckResultReceiver *receiver, luauAnalyze::FrontendWrapper *wrapper, RustString r_name,
+                           RustString r_contents, bool is_module) {
         auto name = from_rust_string(r_name);
         auto contents = from_rust_string(r_contents);
 
         wrapper->file_resolver->register_source(name, std::move(contents), is_module);
         auto result = wrapper->frontend->check(name);
 
-        for (auto& entry : result.errors) push_to_receiver(receiver, entry);
-        for (auto& entry : result.lintResult.errors) push_to_receiver_lint(receiver, name, entry, true);
-        for (auto& entry : result.lintResult.warnings) push_to_receiver_lint(receiver, name, entry, false);
+        for (auto &entry : result.errors)
+            push_to_receiver(receiver, entry);
+        for (auto &entry : result.lintResult.errors)
+            push_to_receiver_lint(receiver, name, entry, true);
+        for (auto &entry : result.lintResult.warnings)
+            push_to_receiver_lint(receiver, name, entry, false);
 
         wrapper->frontend->clear();
     }
 
-    void luauAnalyze_free_frontend(luauAnalyze::FrontendWrapper* wrapper) {
+    void luauAnalyze_free_frontend(luauAnalyze::FrontendWrapper *wrapper) {
         delete wrapper;
     }
 }
